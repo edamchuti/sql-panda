ab1e04ade44b497352780a1145d5d14462668d8b
Emdadul Sadik <einnovtor@gmail.com>
2015-10-01 21:52:36 +0600

    Added del-duplicater.sql script

---
+++ del-duplicate.sql
@@ -0,0 +1,4 @@
+#This sql file upon running finds and eliminates the duplicate entries from 
+# a database
+#Give DB name, table name and how you want to keep one record 
+#eg the most modified one or oldest one etc
